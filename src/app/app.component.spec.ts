a4cc7a14e8efb1f3bf0ebd953241461c87fa5524
mateator <mateatorss@gmail.com>
2022-03-22 16:38:08 +0100

    final commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule,
+        IonicModule.forRoot(),
       ],
       declarations: [
         AppComponent
@@ -20,16 +24,47 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LoginWithTests'`, () => {
+  it('form validation --- invalid', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('LoginWithTests');
+    const component = fixture.componentInstance;
+
+    //triggeo ngOnInit
+    fixture.detectChanges();
+
+    component.loginForm.controls['email'].setValue("a@.com");
+    component.loginForm.controls['password'].setValue("12345");
+
+    expect(component.loginForm.valid).toBeFalse();
+
   });
 
-  it('should render title', () => {
+  it('form validation --- valid', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+
+    //triggeo ngOnInit
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LoginWithTests app is running!');
+
+    component.loginForm.controls['email'].setValue("a@a.com");
+    component.loginForm.controls['password'].setValue("12345");
+
+    expect(component.loginForm.valid).toBeTruthy();
+
+  });
+
+  it(`form should be initialized as empty`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.loginForm.value).toEqual({});
+  });
+
+  it(`toggle should be not checked at start`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    //triggeo ngOnInit
+    fixture.detectChanges();
+
+    const app = fixture.componentInstance;
+    expect(app.loginForm.controls['toggle'].value).toBeFalse();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LoginWithTests';
+export class AppComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      toggle: new FormControl(false),
+    });
+  }
+
+  login(form: FormGroup) {
+    //triggeo el invalid visual de los inputs si no se introducen datos
+    form.markAllAsTouched();
+
+    console.log(form.valid ? "FORM OK" : "FORM NOT OK");
+    console.log("email: " + form.value.email + " --- password: " + form.value.password + " --- toggle: " + form.value.toggle);
+  }
 }
